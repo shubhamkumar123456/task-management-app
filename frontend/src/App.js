cf65c91d3b9d5aa811536a62960fa4a78009231e
shubham kumar <clboy768@gmail.com>
2023-08-28 02:22:38 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,42 @@
+
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Signup from './pages/auth/Signup';
+import Login from './pages/auth/Login';
+import Home from './pages/Home';
+import TaskState from './context/TaskState';
+import { useState } from 'react';
+import Alert from './components/Alert';
+import Navbar from './components/Navbar';
+
+function App() {
+  const token = localStorage.getItem('token');
+  const [alert, setalert] = useState(null);
+  const showAlert=(message,type)=>{
+    setalert({
+      msg:message,
+      type:type
+    })
+    setTimeout(()=>{
+      setalert(null)
+    },1500);
+  }
+  return (
+    <div className="App">
+    <TaskState>
+      <BrowserRouter>
+      <Navbar/>
+      <Alert alert={alert}/>
+      <Routes>
+       {token && <Route path='/' element={<Home showAlert={showAlert}/>}/>}
+       {/* {!token && <Route path='/' element={<Login/>}/>} */}
+        <Route path='/signup' element={<Signup showAlert={showAlert}/>}/>
+        <Route path='/login' element={<Login showAlert={showAlert}/>}/>
+      </Routes>
+      </BrowserRouter>
+      </TaskState>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/auth/Login.jsx
@@ -0,0 +1,58 @@
+import React, { useContext } from 'react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import TaskContext from '../../context/TaskContext'
+
+
+const Login = (props) => {
+  const ctx = useContext(TaskContext)
+  const [credentials, setcredentials] = useState({ email: "", password: "" });
+  let navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch('http://localhost:3001/api/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email: credentials.email, password: credentials.password })
+    });
+    const json = await response.json();
+    console.log(json);
+    if (json.success) {
+      // save the auth token and redirect
+      localStorage.setItem('token', json.authtoken)
+      ctx.getNotes();
+      navigate("/");
+      alert("Logged in successfully")
+      // props.showAlert( "Logged in successfully ","success")
+    } else {
+      alert("invalid details")
+      // props.showAlert( "Invalid details","danger")
+    }
+  }
+  const onchange = (e) => {
+    setcredentials({ ...credentials, [e.target.name]: e.target.value })
+  }
+  return (
+    <div>
+      <h2 className="my-5">Login to iNotebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">Email address</label>
+          <input type="email" className="form-control" id="email" onChange={onchange} value={credentials.email} name='email' aria-describedby="emailHelp" />
+
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">Password</label>
+          <input type="password" className="form-control" onChange={onchange} value={credentials.password} id="password" name='password' />
+        </div>
+
+        <button type="submit" className="btn btn-primary" >Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default Login
