d376c37a351d51d8f3f68796eb5be90fd57d7eb8
shubham kumar <107460088+shubhamkumar123456@users.noreply.github.com>
2023-08-28 18:57:56 +0530

    Update Signup.jsx

--- frontend/src/pages/auth/Signup.jsx
+++ frontend/src/pages/auth/Signup.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import TaskContext from '../../context/TaskContext'
