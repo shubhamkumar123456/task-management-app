49bd1779a6f39eeee009f0853e134e56808d982c
shubham kumar <107460088+shubhamkumar123456@users.noreply.github.com>
2023-08-28 20:27:07 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,15 @@ import { Navigate } from 'react-router-dom';
 import Signup from './pages/auth/Signup';
 import Login from './pages/auth/Login';
 import Home from './pages/Home';
-import TaskState from './context/TaskState';
-import { useState } from 'react';
+
+import { useContext, useState } from 'react';
 import Alert from './components/Alert';
 import Navbar from './components/Navbar';
+import TaskContext from './context/TaskContext';
 
 function App() {
-  const token = localStorage.getItem('token');
+const ctx = useContext(TaskContext)
+
   const [alert, setalert] = useState(null);
   const showAlert=(message,type)=>{
     setalert({
@@ -24,19 +26,27 @@ function App() {
   }
   return (
     <div className="App">
-    <TaskState>
+  
       <BrowserRouter>
       <Navbar/>
       <Alert alert={alert}/>
+
       <Routes>
-     <Route path='/' element={<Home showAlert={showAlert}/>}
+   {ctx.token && <Route path='/' element={<Home showAlert={showAlert}/>}/>}
+   {!ctx.token && <Route path='/' element={<Login showAlert={showAlert}/>}/>}
    
+ 
+   
+
         <Route path='/signup' element={<Signup showAlert={showAlert}/>}/>
-        <Route path='/login' element={<Login showAlert={showAlert}/>}/>
-      
+      <Route path='/login' element={<Login showAlert={showAlert}/>}/>
+      {!ctx.token && <Route path="/" element={ <Navigate to="/login" /> } />}
+      {/* {token && <Route path="/login" element={ <Navigate to="/" /> } />} */}
+  
+        
       </Routes>
       </BrowserRouter>
-      </TaskState>
+      
     </div>
   );
 }
